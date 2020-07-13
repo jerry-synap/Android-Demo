6aebb65bc2f00a2955037d4cc2c7d4b0d80ebece
Jerry <jerry@synapinc.com>
2020-07-13 13:12:50 +0200

    Initial commit

---
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "hpA1Jh9Jr3zYcNQtz-t19pG6s69k67KM";
+	public static String API_SECRET = "0tcxoEFTMkoBoolKOpdewQESxRd7nsq0";
+
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
+}
